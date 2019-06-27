5171f125ea50445ed1e9d52f7d7a94116182a989
Fox <foxdm2@ncats-2073234-m.nih.gov>
2019-06-27 17:45:06 -0400

    saving

--- index.js
+++ index.js
@@ -5,90 +5,60 @@ convertExcel("newList.xlsx", "newJSON.json");
 var newJSON = require("./newJSON.json");
 var _ = require("lodash");
 
-////////////// Setup server so that variables can be inspected in the brower console instead of the IDE console for greater functionality
-const http = require('http')
-const port = 3000
-
-const requestHandler = (request, response) => {
-  console.log(request.url)
-  response.end('Hello Node.js Server!')
+var testProject = {
+  "Report Title": "Developing inhibitors for PIP4Kgamma for Huntington's disease",
+  "NIH Project ID": "",
+  "ZIA ID Number": "TR000207-04",
+  "NCATS Division": "",
+  "2018 Project Status ": "Active",
+  "2019 Project Status ": "",
+  "Lead Investigators": "Marc Ferrer-Alegre (DPCI, NCATS) \nJuan Marugan (DPCI, NCATS) \nNoel Southall (DPCI, NCATS) \n",
+  "Supervisor of Record": "Anton Simeonov (DPCI, NCATS)\n",
+  "NCATS Team Members": "Bolormaa Baljinnyam (DPCI, NCATS)\nMark James Henderson, PhD (DPCI, NCATS)\nSamarjit Patnaik (DPCI, NCATS)\n",
+  "Intramural Collaborators (Affiliation)": "none indicated",
+  "Extramural Collaborators (Affiliation)": "Juan Botas Baylor College of Medicine\nLois Weisman University of Michigan\n",
+  "Does project use human cells, human subjects or human tissues?": "Neither human cells nor tissues\n",
+  "Keywords": "Small molecule; HTS; Screening; HttpolQ",
+  "Distinguishing Keyword": "",
+  "Goals and Objectives": "Using a cell-based assay to search for compounds that lower pathogenic mutant huntingtin protein (mHtt) huntingtin expression, the project team has identified a class of compounds that protect cells via selective inhibition of a lipid kinase, PIP4K. The team is further developing assays in order to study the ability of these compounds to inhibit this lipid kinase and their impact on autophagy and huntingtin protein.",
+  "Summary": "During this period, the project team has further characterized the previously developed lead molecules of interest, and shown that inhibition of PIP4K by a small molecule inhibitor or silencing leads to change in the equilibrium of phosphatidyl ionosotides that increases autophagy and reduce mutant huntingtin protein in human patient fibroblasts and aggregates in neurons. In a Huntington Drosophila model silencing the gene for PIP4K leads to a recovery of motor performance and retinal degeneration, caused by the mutant huntingtin protein. This suggests that PIP4K is a novel pharmacological target for neurodegenerative diseases such as Huntington disease.",
+  "Publications generated during the last reporting period": "1. Al-Ramahi I, Panapakkam Giridharan SS, Chen YC, Patnaik S, Safren N, Hasegawa J, de Haro M, Wagner Gee AK, Titus SA, Jeong H, Clarke J, Krainc D, Zheng W, Irvine RF, Barmada S, Ferrer M, Southall N, Weisman LS, Botas J, Marugan JJ (2017). Inhibition of PIP4Kγ ameliorates the pathological effects of mutant huntingtin protein. Elife 6. https://doi.org/10.7554/eLife.29123"
 }
 
-const server = http.createServer(requestHandler)
+var formatedNamesArray = [];
 
-server.listen(port, (err) => {
-  if (err) {
-    return console.log('something bad happened', err)
+if(testProject['Lead Investigators']) {
+  var rawString = testProject['Lead Investigators'];
+  var preFormattedArray = [];
+  var postFormattedArray = [];
+
+  rawString = rawString.split('\n');
+  if(rawString[0]){
+    preFormattedArray.push(rawString[0]);
+  }
+  if(rawString[1]){
+    preFormattedArray.push(rawString[1]);
+  }
+  if(rawString[2]){
+    preFormattedArray.push(rawString[2]);
+  }
+  if(rawString[3]){
+    preFormattedArray.push(rawString[3]);
   }
 
-  console.log(`server is listening on ${port}`)
-})
-/////////// End server creation ////////////
-
-var ziaIDsOfLeaderErrors = [];
-var ziaIDsOfNCATSteamErrors = [];
-
-originalJSON.forEach(function(element, index) {
-  if (element["ZIA ID Number"] && newJSON[index]) {
-    doLeadersMatch(element, newJSON[index]);
+  for(var i = 0; i < preFormattedArray.length; i++){
+    var regex = /([^\(]+)/;
+    var tempString = preFormattedArray[i].match(regex);
+    postFormattedArray.push(tempString[i]);
   }
-});
 
-function doLeadersMatch(firstProject, secondProject) {
-  var firstProjectLeadsString = firstProject['Lead Investigators'];
-  var secondProjectLeadsString = secondProject['Lead Investigators'];
-  if(firstProjectLeadsString && secondProjectLeadsString) {
-    var firstLeadsArray = stringToArray(firstProjectLeadsString);
-    var secondLeadsArray = stringToArray(secondProjectLeadsString);
-    createNCATSteamArray(firstLeadsArray);
+  for(var i = 0; i < postFormattedArray.length; i++){
+    var fullname = postFormattedArray[i];
+    if(fullname){
+      var fullNameArray = fullname.split(' ');
+      var fullFormattedName = fullNameArray[1] + ', ' + fullNameArray[0];
+      formatedNamesArray.push(fullFormattedName);
+    }
   }
+  console.log(formatedNamesArray);
 }
-
-function getLeader(project) {}
-
-function createNCATSteamArray(unfilteredStrings) {
-  var tempArray = [];
-  unfilteredStrings.forEach(function(element, index){
-    if(index === 0){
-      tempArray.push(element);
-    }
-    if(index === 1){
-      tempArray.push(element);
-    }
-    if(index === 2){
-      tempArray.push(element);
-    }
-  });
-  // ***************************************************!
-  // This suggests that 'tempArray' is an array.
-  console.log(tempArray);
-  // This says that 'tempArray' is an object -> WTF?
-  console.log(typeof tempArray);
-}
-
-function stringToArray(string) {
-  var nameArray = string.split("(");
-  return nameArray;
-}
-
-function getNCATSteamMembers(project) {}
-
-function findNumberofLeaders(projectLeadersArray) {
-}
-
-function findNumberOfWordsInName(name) {}
-
-function doTeamMembersMatch(firstProject, secondProject) {}
-
-function formatOneLine(singleLine) {}
-
-function formatParagraph(paragraph) {}
-
-function format2Names(name) {}
-
-function format3Names(name) {}
-
-function format4Names(name) {}
-
-function format5Names(name) {}
-
