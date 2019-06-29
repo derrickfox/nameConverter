b2f82f6b406a46f64566ba69ec10076605aaeb46
Fox <foxdm2@ncats-2073234-m.fios-router.home>
2019-06-28 20:26:34 -0400

    loops to maps

--- index.js
+++ index.js
@@ -23,11 +23,11 @@ var testProject = {
     "Juan Botas Baylor College of Medicine\nLois Weisman University of Michigan\n",
   "Does project use human cells, human subjects or human tissues?":
     "Neither human cells nor tissues\n",
-  "Keywords": "Small molecule; HTS; Screening; HttpolQ",
+  Keywords: "Small molecule; HTS; Screening; HttpolQ",
   "Distinguishing Keyword": "",
   "Goals and Objectives":
     "Using a cell-based assay to search for compounds that lower pathogenic mutant huntingtin protein (mHtt) huntingtin expression, the project team has identified a class of compounds that protect cells via selective inhibition of a lipid kinase, PIP4K. The team is further developing assays in order to study the ability of these compounds to inhibit this lipid kinase and their impact on autophagy and huntingtin protein.",
-  "Summary":
+  Summary:
     "During this period, the project team has further characterized the previously developed lead molecules of interest, and shown that inhibition of PIP4K by a small molecule inhibitor or silencing leads to change in the equilibrium of phosphatidyl ionosotides that increases autophagy and reduce mutant huntingtin protein in human patient fibroblasts and aggregates in neurons. In a Huntington Drosophila model silencing the gene for PIP4K leads to a recovery of motor performance and retinal degeneration, caused by the mutant huntingtin protein. This suggests that PIP4K is a novel pharmacological target for neurodegenerative diseases such as Huntington disease.",
   "Publications generated during the last reporting period":
     "1. Al-Ramahi I, Panapakkam Giridharan SS, Chen YC, Patnaik S, Safren N, Hasegawa J, de Haro M, Wagner Gee AK, Titus SA, Jeong H, Clarke J, Krainc D, Zheng W, Irvine RF, Barmada S, Ferrer M, Southall N, Weisman LS, Botas J, Marugan JJ (2017). Inhibition of PIP4Kγ ameliorates the pathological effects of mutant huntingtin protein. Elife 6. https://doi.org/10.7554/eLife.29123"
@@ -41,45 +41,39 @@ if (testProject["Lead Investigators"]) {
   var postFormattedArray = [];
 
   rawString = rawString.split("\n");
-  for (var i = 0; i < rawString.length; i++) {
-    if (rawString[i]) {
-      preFormattedArray.push(rawString[i]);
+  rawString.map(element => {
+    if (element) {
+      preFormattedArray.push(element);
     }
-  }
+  });
 
-  for (var i = 0; i < preFormattedArray.length; i++) {
-    var tempString = preFormattedArray[i].match(/([^\(]+)/);
+  preFormattedArray.map(element => {
+    var tempString = element.match(/([^\(]+)/);
     postFormattedArray.push(tempString[0]);
-  }
+  });
 
-  for (var i = 0; i < postFormattedArray.length; i++) {
-    var fullname = postFormattedArray[i];
+  postFormattedArray.map(element => {
+    var fullname = element;
     if (fullname) {
       var fullNameArray = fullname.split(" ");
       var fullFormattedName = fullNameArray[1] + ", " + fullNameArray[0];
       formatedNamesArray.push(fullFormattedName);
     }
-  }
+  });
+
   var finalArray = [];
-  for (var word = 0; word < formatedNamesArray.length; word++) {
-    for (var letter = 0; letter < formatedNamesArray[word].length; letter++) {
-      if (formatedNamesArray[word][letter] === ",") {
-        if (formatedNamesArray[word][letter + 1] === ",") {
-          var tempArr = formatedNamesArray[word].split(",");
-          var tempName = tempArr[2] + ', ' + tempArr[0];
-          if(!_.includes(finalArray, tempName)){
-            tempName = tempName.trim();
-            finalArray.push(tempName);
-          }
+  formatedNamesArray.map(name => {
+    for (var letter = 0; letter < formatedNamesArray.length; letter++) {
+      if (name[letter] === "," && name[letter + 1] === ",") {
+        var tempArr = name.split(",");
+        var tempName = tempArr[2] + ', ' + tempArr[0];
+        if (!_.includes(finalArray, tempName)){
+          tempName = tempName.trim();
+          finalArray.push(tempName);
         }
       }
-      // else if(formatedNamesArray[word][letter + 1] !== ",") {
-      //   if(!_.includes(finalArray, formatedNamesArray[word])){
-      //     finalArray.push(formatedNamesArray[word]);
-      //   }
-      // }
     }
-  }
+  });
 }
 
 console.log("finalArray", finalArray);
